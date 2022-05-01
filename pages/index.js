9bc26084bc67f0e53c25d78ab1a329cfd4fe6a9d
Boya <boyazhang123@gmail.com>
2022-04-30 20:58:16 -0700

    incorporate spotify api

--- pages/index.js
+++ pages/index.js
@@ -8,3 +8,24 @@ export default function Home() {
     </div>
   );
 }
+
+export async function getStaticProps() {
+  const res = await fetch(
+    '	https://api.spotify.com/v1/me/player/recently-played',
+    {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${process.env.SPOTIFY_ACCESS_TOKEN}`,
+      },
+    }
+  );
+  const spotifyData = await res.json();
+
+
+  const artists = spotifyData.items[0].track
+  console.log(spotifyData.items[0].track);
+  return {
+    props: {},
+    revalidate: 120,
+  };
+}
